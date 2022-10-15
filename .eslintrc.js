45027e8b0b6b6796e748d0431aabfba80e879bc4
ivan-pl <pleskaiv@gmail.com>
2022-10-15 19:36:51 +0300

    build: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["standard-with-typescript", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",

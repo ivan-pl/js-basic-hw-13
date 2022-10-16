c5c7b2f4f0f5ee7985d92388ed4058a0285728b0
ivan-pl <pleskaiv@gmail.com>
2022-10-16 11:15:06 +0300

    feat: add task 5

---
+++ src/task5.ts
@@ -0,0 +1,13 @@
+import React from "react";
+// Есть функция которая достает из реакт компонента (любого, и Functional и Class) его defaultProps
+// Нужно заменить FIXME на правильный тип
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FIXME<T> = Partial<T> | undefined;
+
+// Hint: infer
+export const getDefaultProps = <T>(
+  component: React.ComponentType<T>
+): FIXME<T> => {
+  return component.defaultProps;
+};

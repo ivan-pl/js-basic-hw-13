2b1e453afaa17884718a4662e81e92f11ef2057a
ivan-pl <pleskaiv@gmail.com>
2022-10-23 12:07:11 +0300

    feat: CalendarStorage.addEvent

--- src/calendar/calendarStorage.test.ts
+++ src/calendar/calendarStorage.test.ts
@@ -1,7 +1,16 @@
 import { ICalendarStorage, CalendarStorage } from "./calendarStorage";
+import EventRecord from "./types/eventrecord";
+import Status from "./types/status";
+import Tag from "./types/tag";
 
 describe("CalendarStorage", () => {
   let calendarStorage: ICalendarStorage;
+  const event: EventRecord = {
+    date: new Date(),
+    tag: Tag.Work,
+    status: Status.Pending,
+    description: "Some event",
+  };
 
   beforeEach(() => {
     calendarStorage = new CalendarStorage();
@@ -10,6 +19,34 @@ describe("CalendarStorage", () => {
   describe(".addEvent", () => {
     it(".addEvents exists", () => {
       expect(calendarStorage.addEvent).toBeInstanceOf(Function);
+    });
+
+    it("returns Promise", () => {
+      expect(calendarStorage.addEvent(event)).toBeInstanceOf(Promise);
+    });
+
+    it("add id to retuning Event", async () => {
+      const addedEvent = await calendarStorage.addEvent(event);
+      expect(typeof addedEvent.id).toBe("number");
+    });
+
+    it("adds different id's", async () => {
+      const numberOfEvents = 5;
+      const addedEvents = Array.from({ length: numberOfEvents }, () =>
+        calendarStorage.addEvent(event)
+      );
+
+      const idArray = await Promise.all(addedEvents).then((values) =>
+        values.map((_) => _.id)
+      );
+      const setOfId: Set<number> = new Set();
+      idArray.forEach((_) => {
+        if (_ !== undefined) {
+          setOfId.add(_);
+        }
+      });
+
+      expect(setOfId.size).toBe(numberOfEvents);
     });
   });
 });
--- src/calendar/calendarStorage.ts
+++ src/calendar/calendarStorage.ts
@@ -1,19 +1,31 @@
 import EventRecord from "./types/eventrecord";
 import Tag from "./types/tag";
-import Status from "./types/status"
+import Status from "./types/status";
 
 export interface ICalendarStorage {
-  addEvent(event: EventRecord): Promise<EventRecord>,
-  getEventList(id: number): Promise<EventRecord[]>,
-  getEventList(dateFrom: Date, dateTo?: Date): Promise<EventRecord[]>,
-  getEventList(tag: Tag): Promise<EventRecord[]>,
-  getEventList(status: Status): Promise<EventRecord[]>,
-  updateEvent(id:number, newEvent: EventRecord):  Promise<EventRecord>,
-  deleteEvent(id: number): Promise<EventRecord|null>,
+  addEvent(event: EventRecord): Promise<EventRecord>;
+  getEventList(id: number): Promise<EventRecord[]>;
+  getEventList(dateFrom: Date, dateTo?: Date): Promise<EventRecord[]>;
+  getEventList(tag: Tag): Promise<EventRecord[]>;
+  getEventList(status: Status): Promise<EventRecord[]>;
+  updateEvent(id: number, newEvent: EventRecord): Promise<EventRecord>;
+  deleteEvent(id: number): Promise<EventRecord | null>;
 }
 
-export class CalendarStorage implements ICalendarStorage{
+export class CalendarStorage implements ICalendarStorage {
+  private idGenerator = numberGenerator();
+  private storage: EventRecord[] = [];
+
   async addEvent(event: EventRecord): Promise<EventRecord> {
-    return 1
+    const nextId = this.idGenerator.next().value;
+    const newEvent = { ...event, id: nextId };
+    this.storage.push(newEvent);
+    return newEvent;
+  }
+}
+
+function* numberGenerator(): Generator<number> {
+  for (let i = 0; ; i++) {
+    yield i;
   }
 }
